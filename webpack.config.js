4fdaa7d73c4da37963401ab3e2c2cb0f82c57879
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-03 11:39:08 +0300

    setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -34,14 +34,6 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
     ],
   },
   optimization: {
