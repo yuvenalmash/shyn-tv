1e9f5d0c0f1cf96aba64ca66f0e3dc383e0aeaed
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-04 15:10:43 +0300

    add listComents function

--- src/modules/commentsPopup.js
+++ src/modules/commentsPopup.js
@@ -1,16 +1,34 @@
 import getShow from './tvmazeAPI.js';
+import { getComments } from './involvementAPI.js';
 
-const createCommentPopup = async (showId) => {
+export const createCommentPopup = async (showId) => {
   const tvShow = await getShow(showId);
   const popup = `
   <div id="popup">
     <img class="popupImg" src="${tvShow.image.original}" alt="tvShow image">
-    <p>${tvShow.name}</p>
+    <h2>${tvShow.name}</h2>
     <p>${tvShow.genres}</p>
+    <h3>Comments</h3>
+    <ul id="commentslist"></ul>
+    <h3>Add a comment<h3>
+    <input id="userName" type="text" placeholder="Your name">
+    <input id="comment" type="text" placeholder="Your insights">
+    <button onclick="handlePostComment(${showId})">Comment</button>
   </div>`;
   const parent = document.getElementById('commentPopup');
   parent.innerHTML = popup;
   parent.style.display = 'block';
+  listComments(showId)
 };
 
-export default createCommentPopup;
+export const listComments = async (showId) =>{
+  const data = await getComments(showId)
+  console.log(data)
+  const ul = document.getElementById("commentsList")
+  data.forEach(element => {
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(element))
+    ul.appendChild(li);
+  });
+}
+
