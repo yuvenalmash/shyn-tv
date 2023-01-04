c7255629ba78ab2e53e86a1dc75099fe3237a1a3
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-05 01:25:31 +0300

    add addLike fuction

--- src/modules/homePage.js
+++ src/modules/homePage.js
@@ -1,13 +1,13 @@
+import { getLikes } from './involvementAPI.js';
 import getShow from './tvmazeAPI.js';
 
 const createCard = async (showID) => {
   const show = await getShow(showID);
   const card = `
-
     <div class="card" id ="${show.id}">
       <img src="${show.image.medium}" alt="${show.name}">
       <p>${show.name}</p>
-      <div><i class="fa-solid fa-heart fa-lg"></i>  likes</div>
+      <div><a href="#" onclick="handleAddLike(${showID})"><i class="fa-solid fa-heart fa-lg"></a></i> <p id="${showID}likes"></p></div>
       <button class="btn" onclick="createCommentPopup(${showID})">Comments</button>
       <button class="btn">Reservations</button>
     </div>
@@ -15,13 +15,26 @@ const createCard = async (showID) => {
   return card;
 };
 
-const listShows = () => {
+export const listShows = async () => {
   const showIDs = [169, 82, 44, 35, 98, 25];
-  showIDs.forEach(async (element) => {
-    const card = await createCard(element);
-    const parent = document.getElementById('cards');
+  const likesList = await getLikes();
+  const parent = document.getElementById('cards');
+  showIDs.forEach(async (showId) => {
+    const card = await createCard(showId);
     parent.innerHTML += card;
+    const parent2 = document.getElementById(`${showId}likes`);
+    const showLikes = likesList.filter((element) => element.item_id === `${showId}`);
+    if (showLikes[0]) {
+      parent2.innerHTML = `${showLikes[0].likes} likes`;
+    }
   });
 };
 
-export default listShows;
+export const addLikes = async (showId) => {
+  const likesList = await getLikes();
+  console.log(likesList);
+  const showLikes = likesList.filter((element) => element.item_id === `${showId}`);
+  console.log(showLikes[0].likes);
+  const parent = document.getElementById(`${showId}likes`);
+  parent.innerHTML = `${showLikes[0].likes} likes`;
+};
