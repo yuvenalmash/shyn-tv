f0e6ea6830f9c88b45973c8142457ff73322497f
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-04 13:56:41 +0300

    add get and post comments functions

--- src/modules/involvementAPI.js
+++ src/modules/involvementAPI.js
@@ -1,9 +1,9 @@
-const baseURL = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/"
+const baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
 
-const appId = "qS1d1JsU58cIKClC8bN0/"
+const appId = 'qS1d1JsU58cIKClC8bN0/';
 
 // Likes
-const addLike = async (showId) =>{
+export const addLike = async (showId) => {
   const response = await fetch(`${baseURL}/apps/${appId}/likes`, {
     method: 'POST',
     body: JSON.stringify({
@@ -15,12 +15,33 @@ const addLike = async (showId) =>{
   });
   const responseData = await response.json();
   return responseData;
-}
+};
 
-const getLikes = async () =>{
-  const response = await fetch(`${baseURL}${appId}/likes`)
+export const getLikes = async () => {
+  const response = await fetch(`${baseURL}/apps/${appId}/likes`);
   const responseData = await response.json();
   return responseData;
-}
+};
 
+// comments
+export const postComment = async (showId, userName, comment) => {
+  const response = await fetch(`${baseURL}/apps/${appId}/comments`, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: `${showId}`,
+      username: `${userName}`,
+      comment: `${comment}`,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const responseData = await response.json();
+  return responseData;
+};
 
+export const getComment = async (showId) => {
+  const response = await fetch(`${baseURL}/apps/${appId}/comments?item_id=${showId}`);
+  const responseData = await response.json();
+  return responseData;
+};
