5bae7a2a782433d606c69573079bb1e2a48b5938
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-04 12:13:04 +0300

    add comment popup function

--- src/modules/commentsPopup.js
+++ src/modules/commentsPopup.js
@@ -1,11 +1,15 @@
-export const createCommentPopup = () => {
+import { getShow } from "./tvmazeAPI.js"
+
+export const createCommentPopup = async (showId) => {
+  const tvShow = await getShow(showId)
+  console.log(tvShow)
   const popup = `
-    <div>
-      <p>Hello world!</p>
-    </div>
-  `
+  <div id="popup">
+    <img class="popupImg" src="${tvShow.image.original}" alt="tvShow image">
+    <p>${tvShow.name}</p>
+    <p>${tvShow.genres}</p>
+  </div>`
   const parent = document.getElementById('commentPopup');
   parent.innerHTML = popup;
-
-  console.log("Commentpopup");
+  parent.style.display = "block"
 };
