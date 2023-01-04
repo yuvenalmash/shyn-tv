62e4b941dfa3ae9044a734b21177f49cd5bf3cac
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-04 15:10:11 +0300

    add handlePostComment function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import './style.css';
 import listShows from './modules/homePage.js';
-import createCommentPopup from './modules/commentsPopup.js';
+import {createCommentPopup, listComments} from './modules/commentsPopup.js';
+import { postComment } from './modules/involvementAPI.js';
+
+const handlePostComment = async(showId) =>{
+  const userName = document.getElementById("userName").value;
+  const comment = document.getElementById("comment").value;
+  await postComment(showId,userName,comment)
+  listComments(showId)
+}
+
+
 
 listShows();
 window.createCommentPopup = createCommentPopup;
+window.handlePostComment = handlePostComment;
