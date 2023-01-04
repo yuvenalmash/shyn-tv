8d954a70e5e536adb81b48d47e72e140279a2dab
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-05 01:23:29 +0300

    add handleAddLike function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import './style.css';
-import listShows from './modules/homePage.js';
-import {createCommentPopup, listComments} from './modules/commentsPopup.js';
-import { postComment } from './modules/involvementAPI.js';
+import { listShows, addLikes } from './modules/homePage.js';
+import { createCommentPopup, listComments } from './modules/commentsPopup.js';
+import { postLike, postComment } from './modules/involvementAPI.js';
 
-const handlePostComment = async(showId) =>{
-  const userName = document.getElementById("userName").value;
-  const comment = document.getElementById("comment").value;
-  if (userName !== '' && comment !== ''){
-    await postComment(showId,userName,comment)
-    listComments(showId)
+const handlePostComment = async (showId) => {
+  const userName = document.getElementById('userName').value;
+  const comment = document.getElementById('comment').value;
+  if (userName !== '' && comment !== '') {
+    await postComment(showId, userName, comment);
+    listComments(showId);
   }
-}
+};
 
-
+const handleAddLike = async (showId) => {
+  await postLike(showId);
+  addLikes(showId);
+};
 
 listShows();
 window.createCommentPopup = createCommentPopup;
 window.handlePostComment = handlePostComment;
+window.handleAddLike = handleAddLike;
