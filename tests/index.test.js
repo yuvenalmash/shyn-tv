de6570951154bdfc2240e49e9871ddd731faf985
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-05 14:40:26 +0300

    add comment counter test

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,3 +3,11 @@
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum(1, 2)).toBe(3);
 // });
+
+
+import { commentCounter } from '../src/modules/commentsPopup.js'
+
+test('checks if length of [{a:2},{b:3},{c:4}] is 3',()=>{
+  const comments = [{a:2},{b:3},{c:4}]
+  expect(commentCounter(comments)).toBe(3);
+})
