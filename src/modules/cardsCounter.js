be93d507f71c62f98a6d4b17a99e1f30617f09a4
Adeshina Adenuga <nuga0909@gmail.com>
2023-01-05 08:10:10 +0100

    Add cardsCounter js file

---
+++ src/modules/cardsCounter.js
@@ -0,0 +1,15 @@
+class Shows {
+    constructor(number) {
+      this.number = number;
+    }
+  
+    getNum() {
+      return this.number;
+    }
+  
+    setNum(index) {
+      this.number = index;
+    }
+  }
+  
+  export default Shows;
