eeeb791b91e5d16343b5f3a8469f560dadb0df12
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-05 15:44:42 +0300

    add tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,16 @@
-// import { sum } from '../src/index.js';
+import { commentCounter } from '../src/modules/commentsPopup.js';
+import { featuredShowsCounter } from '../src/modules/homePage.js';
 
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(sum(1, 2)).toBe(3);
-// });
+describe('Comments counter', () => {
+  test('checks if length of [{a:2},{b:3},{c:4}] is 3', () => {
+    const comments = [{ a: 2 }, { b: 3 }, { c: 4 }];
+    expect(commentCounter(comments)).toBe(3);
+  });
+});
 
-
-import { commentCounter } from '../src/modules/commentsPopup.js'
-
-test('checks if length of [{a:2},{b:3},{c:4}] is 3',()=>{
-  const comments = [{a:2},{b:3},{c:4}]
-  expect(commentCounter(comments)).toBe(3);
-})
+describe('Featured shows counter', () => {
+  test('checks if length of [1,2,3,4,5] is 5', () => {
+    const shows = [1, 2, 3, 4, 5];
+    expect(featuredShowsCounter(shows)).toBe(5);
+  });
+});
