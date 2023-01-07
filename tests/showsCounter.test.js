0064ea7044b38c7936c5e12af15150196c9c900d
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-07 03:27:32 +0300

    add tests for shows counter

--- tests/showsCounter.test.js
+++ tests/showsCounter.test.js
@@ -1,8 +1,21 @@
-import { featuredShowsCounter } from '../src/modules/homePage.js';
+/**
+ * @jest-environment jsdom
+ */
+import featuredShowsCounter from '../src/modules/showsCounter.js';
 
 describe('Featured shows counter', () => {
-  test('checks if length of [1,2,3,4,5] is 5', () => {
-    const shows = [1, 2, 3, 4, 5];
-    expect(featuredShowsCounter(shows)).toBe(5);
+  test('checks if number of items with class name is 3', () => {
+    document.body.innerHTML = '<div>'
+      + '<div class=\'card\'>card</div>'
+      + '<div class=\'card\'>card</div>'
+      + '<div class=\'card\'>card</div>'
+      + '</div>';
+    expect(featuredShowsCounter('card')).toBe(3);
+  });
+
+  test('checks if number of items with class name is 0', () => {
+    document.body.innerHTML = '<div>'
+      + '</div>';
+    expect(featuredShowsCounter('card')).toBe(0);
   });
 });
