7d7de99bd37d1e1adf63986f4db402d486ae4ef1
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-08 19:59:47 +0300

    add searchshow fuctions

---
+++ src/modules/searchPage.js
@@ -0,0 +1,18 @@
+import { searchShow } from "./tvmazeAPI.js";
+import { createCard } from "./homePage.js";
+
+export const searchedShows = async(query) =>{
+  const shows = await searchShow(query)
+  console.log(shows)
+} 
+
+export const listSearchedShows = async(query) =>{
+  const shows = await searchShow(query)
+  const container = document.getElementById("cards")
+  container.innerHTML = ""
+  shows.forEach(async(element) => {
+    const card = await createCard(element.show.id);
+    // container.appendChild(card)
+    container.innerHTML += card
+  });
+}
