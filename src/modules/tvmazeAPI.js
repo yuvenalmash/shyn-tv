44a932257bede1ebb97be9ca4b936c8f0515f9ad
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-08 19:59:07 +0300

    add searchshow function

--- src/modules/tvmazeAPI.js
+++ src/modules/tvmazeAPI.js
@@ -1,9 +1,13 @@
-const baseURL = 'https://api.tvmaze.com/shows/';
+const baseURL = 'https://api.tvmaze.com';
 
-const getShow = async (showID) => {
-  const response = await fetch(`${baseURL}${showID}`);
+export const getShow = async (showID) => {
+  const response = await fetch(`${baseURL}/shows/${showID}`);
   const responseData = await response.json();
   return responseData;
 };
 
-export default getShow;
+export const searchShow = async (query) => {
+  const response = await fetch(`${baseURL}/search/shows?q=${query}`)
+  const responseData = await response.json()
+  return responseData
+}
